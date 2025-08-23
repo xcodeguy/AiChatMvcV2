2ad10f44d0e30b5e83473957299f78043d9ae547
David Ferrell <xcodeguy@xcodeguy.com>
2025-08-22 21:36:34 -0400

    added font awesome

--- AiChatMvcV2/wwwroot/js/site.js
+++ AiChatMvcV2/wwwroot/js/site.js
@@ -46,6 +46,14 @@ $(document).ready(function () {
     var TimeElapsedCalculatedSeconds;
     var lastElapsedTime;
 
+    $('.btn-plus, .btn-minus').on('click', function (e) {
+        const isNegative = $(e.target).closest('.btn-minus').is('.btn-minus');
+        const input = $(e.target).closest('.input-group').find('input');
+        if (input.is('input')) {
+            input[0][isNegative ? 'stepDown' : 'stepUp']()
+        }
+    });
+
     $("#btnLlmSettings").click(function () {
     });
 
